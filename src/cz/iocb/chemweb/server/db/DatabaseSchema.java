fc6e751f93a5edb6c86a1f331e971662f1b332fe
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-05-21 20:38:41 +0200

    Add classes describing the database schema

---
+++ src/cz/iocb/chemweb/server/db/DatabaseSchema.java
@@ -0,0 +1,116 @@
+package cz.iocb.chemweb.server.db;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+
+
+public abstract class DatabaseSchema
+{
+    public static class KeyPair
+    {
+        private final String parent;
+        private final String foreign;
+
+
+        public KeyPair(String parent, String foreign)
+        {
+            this.parent = parent;
+            this.foreign = foreign;
+        }
+
+
+        @Override
+        public int hashCode()
+        {
+            return parent.hashCode() ^ foreign.hashCode();
+        }
+
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if(this == obj)
+                return true;
+
+            if(obj == null || !(obj instanceof KeyPair))
+                return false;
+
+            KeyPair other = (KeyPair) obj;
+
+            return parent.equals(other.parent) && foreign.equals(other.foreign);
+        }
+
+
+        public final String getParent()
+        {
+            return parent;
+        }
+
+
+        public final String getForeign()
+        {
+            return foreign;
+        }
+    }
+
+
+    protected final HashMap<String, List<List<String>>> primaryKeys = new HashMap<String, List<List<String>>>();
+    protected final HashMap<KeyPair, List<List<KeyPair>>> foreignKeys = new HashMap<KeyPair, List<List<KeyPair>>>();
+
+
+    public List<String> getCompatibleKey(String table, List<String> columns)
+    {
+        List<List<String>> keys = primaryKeys.get(table);
+
+        if(keys == null)
+            return null;
+
+        loop:
+        for(List<String> key : keys)
+        {
+            for(String part : key)
+                if(!columns.contains(part))
+                    continue loop;
+
+            return key;
+        }
+
+        return null;
+    }
+
+
+    public List<KeyPair> getCompatibleForeignKey(String parentTable, String foreignTable, ArrayList<KeyPair> columns,
+            LinkedHashSet<String> parentColumns)
+    {
+        List<List<KeyPair>> keys = getForeignKeys(parentTable, foreignTable);
+
+        if(keys == null)
+            return null;
+
+
+        loop:
+        for(List<KeyPair> key : keys)
+        {
+            for(KeyPair keyPair : key)
+                if(!columns.contains(keyPair))
+                    continue loop;
+
+            for(String parentColumn : parentColumns)
+                if(key.stream().noneMatch(keyPair -> keyPair.parent.equals(parentColumn)))
+                    continue loop;
+
+            return key;
+        }
+
+        return null;
+    }
+
+
+    public List<List<KeyPair>> getForeignKeys(String parentTable, String foreignTable)
+    {
+        return foreignKeys.get(new KeyPair(parentTable, foreignTable));
+    }
+}
---
+++ src/cz/iocb/chemweb/server/db/postgresql/PostgresSchema.java
@@ -0,0 +1,106 @@
+package cz.iocb.chemweb.server.db.postgresql;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import cz.iocb.chemweb.server.db.DatabaseSchema;
+
+
+
+public class PostgresSchema extends DatabaseSchema
+{
+    private static PostgresSchema singleton = null;
+
+
+    private PostgresSchema() throws FileNotFoundException, SQLException, IOException
+    {
+        try (Connection connection = ConnectionPool.getConnection())
+        {
+            DatabaseMetaData metaData = connection.getMetaData();
+
+            try (ResultSet tables = metaData.getTables(null, null, null, new String[] { "TABLE", "VIEW" }))
+            {
+                while(tables.next())
+                {
+                    String table = tables.getString("TABLE_NAME");
+                    List<List<String>> pkeys = new ArrayList<List<String>>();
+
+                    try (ResultSet indexes = metaData.getIndexInfo(null, null, table, true, false))
+                    {
+                        List<String> columns = null;
+
+                        while(indexes.next())
+                        {
+                            short position = indexes.getShort("ORDINAL_POSITION");
+
+                            if(position == 0)
+                                continue;
+
+                            if(position == 1)
+                            {
+                                columns = new ArrayList<String>();
+                                pkeys.add(columns);
+                            }
+
+                            columns.add(indexes.getString("COLUMN_NAME"));
+                        }
+                    }
+
+                    primaryKeys.put(table, pkeys);
+
+
+                    try (ResultSet indexes = metaData.getCrossReference(null, null, table, null, null, null))
+                    {
+                        List<List<KeyPair>> fkeys = null;
+                        List<KeyPair> columns = null;
+                        String lastFkTable = null;
+
+                        while(indexes.next())
+                        {
+                            short seq = indexes.getShort("KEY_SEQ");
+
+                            if(seq == 1)
+                            {
+                                String fktable = indexes.getString("FKTABLE_NAME");
+
+                                if(!fktable.equals(lastFkTable))
+                                {
+                                    fkeys = new ArrayList<List<KeyPair>>();
+                                    foreignKeys.put(new KeyPair(table, fktable), fkeys);
+                                    lastFkTable = fktable;
+                                }
+
+                                columns = new ArrayList<KeyPair>();
+                                fkeys.add(columns);
+                            }
+
+
+                            columns.add(new KeyPair(indexes.getString("PKCOLUMN_NAME"),
+                                    indexes.getString("FKCOLUMN_NAME")));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static PostgresSchema get() throws FileNotFoundException, SQLException, IOException
+    {
+        if(singleton != null)
+            return singleton;
+
+        synchronized(PostgresSchema.class)
+        {
+            if(singleton == null)
+                singleton = new PostgresSchema();
+
+            return singleton;
+        }
+    }
+}
