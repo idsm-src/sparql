632fa785171751d7858a6039f1a92db379ba33f5
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-05-27 22:59:47 +0200

    Remove files related to obsoleted SPARQL translation implementation

--- src/cz/iocb/chemweb/server/sparql/translator/config/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing classes for procedure calls configuration files.
- */
-package cz.iocb.chemweb.server.sparql.translator.config;
-
--- src/cz/iocb/chemweb/server/sparql/translator/visitor/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing classes for the main translation process by visitor design pattern.
- */
-package cz.iocb.chemweb.server.sparql.translator.visitor;
-
