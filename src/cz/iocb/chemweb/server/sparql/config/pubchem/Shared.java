4cfa3277ada1fb045c189355de956cd5f879c8d0
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-12-06 22:00:30 +0100

    Update mesh regex patterns

--- src/cz/iocb/chemweb/server/sparql/config/pubchem/Shared.java
+++ src/cz/iocb/chemweb/server/sparql/config/pubchem/Shared.java
@@ -14,9 +14,9 @@ class Shared
                         + "gene|inchikey|measuregroup|ontology|protein|reference|source|substance|synonym|descriptor/(compound|substance))"));
 
         config.addIriClass(new UserIriClass("dqmesh", Arrays.asList("integer", "integer"),
-                "http://id.nlm.nih.gov/mesh/D[0-9]+(Q[0-9]+)?"));
+                "http://id.nlm.nih.gov/mesh/D[0-9]{6}(Q[0-9]{6})?"));
         config.addIriClass(
-                new UserIriClass("mesh", Arrays.asList("integer"), "http://id.nlm.nih.gov/mesh/(C|M)[0-9]+"));
+                new UserIriClass("mesh", Arrays.asList("integer"), "http://id.nlm.nih.gov/mesh/(C[0-9]{9}|M[0-9]{7})"));
         config.addIriClass(new UserIriClass("pdblink", Arrays.asList("char(4)"), "http://rdf.wwpdb.org/pdb/.*"));
         config.addIriClass(new UserIriClass("uniprot", Arrays.asList("varchar"), "http://purl.uniprot.org/uniprot/.*"));
     }
