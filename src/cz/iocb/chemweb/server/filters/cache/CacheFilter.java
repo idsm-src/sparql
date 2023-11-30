4acc9161914ff9dea6680397d9eacce6a799daac
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2023-11-30 15:20:33 +0100

    Extend cache filter to support list of excluded paths

--- src/cz/iocb/chemweb/server/filters/cache/CacheFilter.java
+++ src/cz/iocb/chemweb/server/filters/cache/CacheFilter.java
@@ -1,39 +1,55 @@
 package cz.iocb.chemweb.server.filters.cache;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 
 public class CacheFilter implements Filter
 {
-    @SuppressWarnings("unused") private FilterConfig filterConfig;
+    private FilterConfig filterConfig;
+    private HashSet<String> excludedUrls = new HashSet<String>();
+
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
             throws IOException, ServletException
     {
+        String path = ((HttpServletRequest) request).getServletPath();
 
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        if(!excludedUrls.contains(path))
+        {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        httpResponse.setHeader("Pragma", "no-cache");
-        httpResponse.setDateHeader("Expires", 0);
+            httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+            httpResponse.setHeader("Pragma", "no-cache");
+            httpResponse.setDateHeader("Expires", 0);
+        }
 
         filterChain.doFilter(request, response);
     }
+
 
     @Override
     public void init(FilterConfig _filterConfig) throws ServletException
     {
         filterConfig = _filterConfig;
+
+        String excludes = filterConfig.getInitParameter("excludedUrls");
+
+        if(excludes != null)
+            excludedUrls.addAll(Set.of(excludes.split(" ")));
     }
+
 
     @Override
     public void destroy()
